07e5435c50080157aa3bb0224e7ba7fc87e58048
Stephan Watermeyer <m@phrk.de>
2020-02-12 17:40:14 +0100

    Renamed index.html with index.php

---
+++ config.inc.example.php
@@ -0,0 +1,10 @@
+<?php
+define('VEREINSNAME', '');
+define('CANONICAL_URL', '');
+define('CONFIRM_URL', '');
+define('DATENSCHUTZ_IMPRESSUM_URL', '');
+define('SEPA_GLAEUBIGER_ID', '');
+define('ABSENDER_MAILADRESSE', '');
+define('RECAPTCHA_KEY', '');
+
+?>
